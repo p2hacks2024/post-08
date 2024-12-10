9ba1f69b67c81e700259567743e1770e75075f41
ayanentity <chikinnanman14@gmail.com>
2024-12-10 23:18:11 +0900

    フッター仮完成

---
+++ pages/top.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="bg">
+    <div
+      class="min-h-screen flex flex-col items-center justify-center"
+    >
+      <h1 class="text-4xl font-extrabold text-blue-600">POST-8へようこそ</h1>
+      <p class="mt-4 text-gray-700 text-lg">
+        これはNuxt.jsとTailwind CSSを使った簡単なホームページです。
+      </p>
+      <Footer></Footer>
+    </div>
+    </div>
+  </template>
+  
+  <style>
+    .bg {
+      background-image: url("@/assets/images/bg-hoshisen.png");
+      background-size: cover; /* 画像をコンテナのサイズに合わせて拡大・縮小 */
+      background-position: center center; /* 画像の位置を中央に設定 */
+      background-repeat: no-repeat; /* 画像が繰り返し表示されないように設定 */
+      height: 100%; 
+    }
+  </style>
+  
+  <script setup>
+  const handleButtonClick = () => {
+    alert("POST-8にようこそ");
+  };
+  </script>
+  
