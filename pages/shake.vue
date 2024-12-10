6e8e570fc9b8b891eb6416c7cb2b308cc82415e6
Sakamoto <b1022107@fun.ac.jp>
2024-12-10 19:06:58 +0900

    iPhoneのときに許可を得るようにしたよ

--- pages/shake.vue
+++ pages/shake.vue
@@ -9,19 +9,33 @@
 export default {
     data() {
         return {
-            shakeStrength: 0,   //shakeの強さ
+            shakeStrength: 0,   // shakeの強さ
             lastX: null,
             lastY: null,
-            lastZ: null,    //前回のXYZ
-            lastUpdate: 0,  //最終更新時間
-            updateInterval: 100, //更新間隔(ms)
+            lastZ: null,    // 前回のXYZ座標
+            lastUpdate: 0,  // 最終更新時間
+            updateInterval: 100, // 更新間隔（ミリ秒）
         };
     },
-    mounted() {     //devicemotionが使えるかどうかみる
-        if (window.DeviceMotionEvent) {
-            window.addEventListener('devicemotion', this.handleMotion);
+    mounted() {     // devicemotionが使えるかどうか確認
+        if (typeof DeviceMotionEvent.requestPermission === 'function') {
+            // iOS 13+ の場合、ユーザーに許可を求める
+            DeviceMotionEvent.requestPermission()
+                .then(permissionState => {
+                    if (permissionState === 'granted') {
+                        window.addEventListener('devicemotion', this.handleMotion);
+                    } else {
+                        alert("デバイスモーションの許可が得られませんでした。");
+                    }
+                })
+                .catch(console.error);
         } else {
-            alert("お使いのデバイスではサポートされていません。");
+            // その他のブラウザの場合
+            if (window.DeviceMotionEvent) {
+                window.addEventListener('devicemotion', this.handleMotion);
+            } else {
+                alert("お使いのデバイスではサポートされていません。");
+            }
         }
     },
     beforeDestroy() {
@@ -29,17 +43,17 @@ export default {
     },
     methods: {
         handleMotion(event) {
-            const currentTime = Date.now();     //現在の時間をミリ秒単位で取得
-            if (currentTime - this.lastUpdate < this.updateInterval) {  //lastUpdate) の差が updateInterval 未満であれば、処理をスキップ
+            const currentTime = Date.now();     // 現在の時間をミリ秒単位で取得
+            if (currentTime - this.lastUpdate < this.updateInterval) {  // lastUpdate の差が updateInterval 未満であれば、処理をスキップ
                 return; 
             }
             this.lastUpdate = currentTime;
 
-            //accelerationIncludingGravity プロパティを取得し、x, y, z の各軸の加速度データを抽出
+            // accelerationIncludingGravity プロパティを取得し、x, y, z の各軸の加速度データを抽出
             const { acceleration } = event;
             const { x, y, z } = acceleration;
 
-            //前回の加速度データ (lastX, lastY, lastZ) が存在する場合、現在の加速度データとの変化量 (deltaX, deltaY, deltaZ) を計算
+            // 前回の加速度データ (lastX, lastY, lastZ) が存在する場合、現在の加速度データとの変化量 (deltaX, deltaY, deltaZ) を計算
             if (this.lastX !== null && this.lastY !== null && this.lastZ !== null) {
                 const deltaX = Math.abs(this.lastX - x);
                 const deltaY = Math.abs(this.lastY - y);
