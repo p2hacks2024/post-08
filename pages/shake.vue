a88eb0b1b52091aa6ea086f1073fc007726def1a
Sakamoto <b1022107@fun.ac.jp>
2024-12-10 18:56:34 +0900

    accelerationIncludingGravityをaccelerationに変更

--- pages/shake.vue
+++ pages/shake.vue
@@ -9,17 +9,19 @@
 export default {
     data() {
         return {
-            shakeStrength: 0,
+            shakeStrength: 0,   //shakeの強さ
             lastX: null,
             lastY: null,
-            lastZ: null,
+            lastZ: null,    //前回のXYZ
+            lastUpdate: 0,  //最終更新時間
+            updateInterval: 100, //更新間隔(ms)
         };
     },
-    mounted() {
+    mounted() {     //devicemotionが使えるかどうかみる
         if (window.DeviceMotionEvent) {
             window.addEventListener('devicemotion', this.handleMotion);
         } else {
-            alert("DeviceMotionEvent is not supported on your device.");
+            alert("お使いのデバイスではサポートされていません。");
         }
     },
     beforeDestroy() {
@@ -27,9 +29,17 @@ export default {
     },
     methods: {
         handleMotion(event) {
-            const { accelerationIncludingGravity } = event;
-            const { x, y, z } = accelerationIncludingGravity;
+            const currentTime = Date.now();     //現在の時間をミリ秒単位で取得
+            if (currentTime - this.lastUpdate < this.updateInterval) {  //lastUpdate) の差が updateInterval 未満であれば、処理をスキップ
+                return; 
+            }
+            this.lastUpdate = currentTime;
 
+            //accelerationIncludingGravity プロパティを取得し、x, y, z の各軸の加速度データを抽出
+            const { acceleration } = event;
+            const { x, y, z } = acceleration;
+
+            //前回の加速度データ (lastX, lastY, lastZ) が存在する場合、現在の加速度データとの変化量 (deltaX, deltaY, deltaZ) を計算
             if (this.lastX !== null && this.lastY !== null && this.lastZ !== null) {
                 const deltaX = Math.abs(this.lastX - x);
                 const deltaY = Math.abs(this.lastY - y);
