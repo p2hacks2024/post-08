433528fa8d5fb5b6ec23782e6580fa803e998e74
kiguruu <171789840+kiguruu@users.noreply.github.com>
2024-12-14 13:11:54 +0900

    Update: Delete unused function in index.vue.

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,11 @@
 <template>
   <div class="bg">
-   
-  
   <div
     class="min-h-screen flex flex-col items-center justify-center"
   >
        <router-link to="/login">
     <button
       class="mt-6 px-6 py-2 bg-orange-400 text-white rounded-full shadow-md hover:bg-yellow-400"
-      @click="handleButtonClick"
     >
       Xのアカウントを連携して始める
     </button>
@@ -24,10 +21,10 @@
     background-size: cover; /* 画像をコンテナのサイズに合わせて拡大・縮小 */
     background-position: center center; /* 画像の位置を中央に設定 */
     background-repeat: no-repeat; /* 画像が繰り返し表示されないように設定 */
-    height: 100%; 
+    height: 100%;
   }
 
-  
+
 </style>
 
 <script setup>
