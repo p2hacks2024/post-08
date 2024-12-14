11d6a9abdd832da6d94f6118a4c01dcfc828a8e6
kiguruu <171789840+kiguruu@users.noreply.github.com>
2024-12-14 18:43:57 +0900

    Update: Change the appearance of the flash page.

--- pages/flash/[id].vue
+++ pages/flash/[id].vue
@@ -1,27 +1,35 @@
 <template>
-  <div
-    class="min-h-screen flex flex-col items-center justify-center"
-  >
-    <h1 class="text-4xl font-extrabold text-blue-600">あなたの願いを星にしよう！</h1>
-    <div class="mt-2">
-      <img
-        :src="`https://pub-0afe10dfcf4442e4a38f75c617de9147.r2.dev/canvas_image_${imageID}.png`"
-        alt="短冊の画像"
-        class="rounded-lg shadow-md"
-      />
-    </div>
-    <button
-      class="mt-6 px-6 py-2 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-600"
-      @click="changeStatus"
+  <div class="bg">
+    <div
+      class="min-h-screen flex flex-col items-center justify-center pb-[20vh]"
     >
-      星にする
-    </button>
-    <div v-if="user_status" class="flex flex-row mt-4 text-gray-700 text-lg">
-      <p class="mr-2">現在のステータスは:</p>
-      <p>{{ user_status }}</p>
-      <p class="ml-2">です</p>
+      <h1 class="text-4xl font-extrabold text-amber-500">願いは叶った？</h1>
+      <h2 class="text-2xl font-semibold text-white mt-6">叶った願いを星にしよう！</h2>
+      <div class="mt-2">
+        <img
+          :src="`https://pub-0afe10dfcf4442e4a38f75c617de9147.r2.dev/canvas_image_${imageID}.png`"
+          alt="短冊の画像"
+          class="rounded-lg shadow-md bg-white mt-2"
+        />
+      </div>
+      <div v-if="user_status == 'none'" class="flex flex-row mt-4 text-white text-lg">
+        <p class="mr-2">現在のステータスは:</p>
+        <p>{{ user_status }}</p>
+        <p class="ml-2">です</p>
+        <button
+          class="mt-6 px-6 py-2 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-600"
+          @click="changeStatus"
+        >
+          星にする
+        </button>
+      </div>
+      <div v-else-if="user_status == 'flash'" class="flex flex-row mt-4 text-white text-lg">
+        <p class="mr-2">現在のステータスは:</p>
+        <p>{{ user_status }}</p>
+        <p class="ml-2">です</p>
+      </div>
+      <p v-else class="mt-4 text-white text-lg">ステータスが読み取れませんでした。</p>
     </div>
-    <p v-else class="mt-4 text-gray-700 text-lg">ステータスが読み取れませんでした。</p>
   </div>
 </template>
 
@@ -41,7 +49,6 @@ async function getUserData() {
   try {
     const data = await useFirestore().getUserData(userID.value);
     user_status.value = data?.status;
-    console.log(data?.imageID)
     console.log('status: ', user_status.value)
     return user_status;
   } catch(e){
@@ -73,3 +80,13 @@ onMounted(async () => {
 })
 
 </script>
+
+<style>
+.bg {
+  background-image: url("@/assets/images/bg-hoshisen.png");
+  background-size: cover;
+  background-position: center center;
+  background-repeat: no-repeat;
+  height: 100%;
+}
+</style>
